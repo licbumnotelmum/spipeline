02aafbc8b08b647f8c89dfb3c9084be8b9839c36
licbumnotelmum <nealkuril1@gmail.com>
2026-01-02 23:05:24 +0530

    added single song download option

---
+++ sing_let.py
@@ -0,0 +1,10 @@
+import webpaige as ww
+# name = input("\nEnter the name of the song\n")
+# artist = input("\nEnter the artist of the song\n")
+
+name = 'creep'
+artist = 'radiohead'
+
+
+ww.downloadCunt([dict(name = name , artists = artist)])
+print("completed")
